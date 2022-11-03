47c8222ae04a868e4dcb31f05dcd9b15cc61a393
ybw0014 <i@guizhanss.com>
2022-11-03 01:14:34 -0400

    修复引用

--- src/main/java/net/guizhanss/networks/BlockCache.java
+++ src/main/java/net/guizhanss/networks/BlockCache.java
@@ -1,7 +1,7 @@
 package net.guizhanss.networks;
 
-import dev.sefiraat.netheopoiesis.dough.blocks.BlockPosition;
 import io.github.thebusybiscuit.slimefun4.implementation.Slimefun;
+import io.github.thebusybiscuit.slimefun4.libraries.dough.blocks.BlockPosition;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.protection.Interaction;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
